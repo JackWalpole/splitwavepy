f4b6d467a43c854bf2bf9c2fafa1ee6da78302fa
Jack Walpole <j.walpole@gmail.com>
2017-09-17 19:51:00 +0100

    Started adding error surface stacking capability

---
+++ splitwavepy/eigval/stack.py
@@ -0,0 +1,63 @@
+"""
+The eigenvalue method of Silver and Chan (1991)
+Uses Pair to do high level work
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# from ..core import core
+# from ..core import pair
+# from ..core import window
+from . import EigenM
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Stack:
+    
+    def __init__(self,*args,**kwargs):
+        """
+        Stack list of EigenM measurements.
+        
+        ----------------------------------
+        *args 
+        
+        list of EigenM instances
+        ----------------------------------
+        **kargs
+        
+        weights = numpy array of weights (order must match list)
+        ----------------------------------
+        """
+        
+        if not isinstance(args[0],list):
+            raise Exception('requires a list of EigenM measurements as the first positional argument')
+        
+        # list of measurements  
+        self.list = args[0]
+        
+        # numpy array of weights
+        if len(args) > 1 and 
+            isinstance(args[1],np.ndarray) and
+                args[1].size == len(self.list):            
+            self.weights = args[1]
+        else:
+            self.weights = np.ones(len(self.list))
+        
+        # stacking routine
+        def stack(self,**kwargs):
+            """
+            Return numpy array of stacked lam1/lam2 values
+            
+            **kwargs
+            """  
+            stack = np.zeros(listM[0].degs.shape)            
+            for ii in range(len(self.list)):
+                m = self.list[ii]         
+                stack += self.weight[ii] * (m.lam1/m.lam2)
+            # 
+            return stack / np.sum(self.weights)
+
+
