f9611700804f518f060d63cea1a75b0d1c847fa7
Jack Walpole <j.walpole@gmail.com>
2017-09-21 15:36:33 +0100

    Changed __init_ files to put Stack in more sensible namespace

--- splitwavepy/__init__.py
+++ splitwavepy/__init__.py
@@ -6,7 +6,6 @@ from __future__ import print_function
 from .core.pair import Pair
 from .core.trio import Trio
 from .eigval.eigenM import EigenM
-from .eigval.stack import Stack
 from .core import core
 from .core import core3d
 from .core.window import Window
--- splitwavepy/eigval/__init__.py
+++ splitwavepy/eigval/__init__.py
@@ -1,1 +1,1 @@
-from . import stack
+from .stack import Stack
