0d60f97d4f3e285ff191db718967b47d86e995c3
Jack <jack.walpole@bristol.ac.uk>
2017-10-03 12:45:31 +0100

    Fixed setup.py and README.md

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 _Easily measure shear wave splitting using python._
 
-Jack Walpole
+*Jack Walpole*
 
 github: https://github.com/JackWalpole/splitwavepy
 
-install: download from github and use `pip install splitwavepy` from the folder holding the download.
+install: `pip install splitwavepy`.
 
 documentation: http://splitwavepy.readthedocs.io/
 
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name = 'splitwavepy',
+    name = 'SplitWavePy',
     version = '0.1.0',
     description = "Shearwave splitting measurement tools",
     author = 'Jack Walpole',
     author_email = 'j.walpole@gmail.com',
     license = 'MIT',
     url = 'https://github.com/JackWalpole/splitwavepy',
-    packages = ['splitwavepy'],
+    packages = find_packages(),
     install_requires = [
         'matplotlib >= 1.1',
         'numpy >= 1.1',
