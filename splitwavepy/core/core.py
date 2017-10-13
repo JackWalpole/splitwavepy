Fix rotation issue

splitwavepy/core/core.py
@@ -66,10 +66,10 @@ def rotate(x,y,degrees):
     """row 0 is x-axis and row 1 is y-axis,
        rotates from x to y axis
        e.g. N to E if row 0 is N cmp and row1 is E cmp"""
-    ang = math.degrees(degrees)
-    rot = np.array([[np.cos(ang),np.sin(ang)],
+    ang = math.radians(degrees)
+    rot = np.array([[ np.cos(ang), np.sin(ang)],
                     [-np.sin(ang), np.cos(ang)]])
-    xy = np.dot(rot,np.vstack((x,y)))
+    xy = np.dot(rot, np.vstack((x,y)))
     return xy[0], xy[1]
 
 def split(x,y,degrees,samps):

splitwavepy/core/pair.py
@@ -7,6 +7,7 @@ from . import core, geom, io
 from .window import Window
 
 import numpy as np
+import math
 from scipy import signal
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
@@ -152,21 +153,21 @@ class Pair:
         Rotate data so that trace1 lines up with *degrees*
         """
         # find appropriate rotation matrix
-        ang = np.deg2rad(degrees)
+        ang = math.radians(degrees)
         # define the new cmpvecs
         backoff = self.cmpvecs.T
-        self.cmpvecs = np.array([[np.cos(ang),-np.sin(ang)],
-                            [np.sin(ang), np.cos(ang)]])
+        self.cmpvecs = np.array([[ math.cos(ang), math.sin(ang)],
+                                 [-math.sin(ang), math.cos(ang)]])
         # find the rotation matrix. 
         # Linear algebra: if a and b are rotation matrices, 
         # then: a.T = inv(a) and b.T = inv(b)
         # then: dot(a.T,a) = dot(b.T,b) = I
         # and (multiply by b): dot(dot(b,a.T),a) = b.
         # i.e., dot(b,a.T) is the rotation matrix that converts a to b.
-        rot = np.dot(self.cmpvecs,backoff)
+        rot = np.dot(self.cmpvecs, backoff)
         # rotate data
-        xy = np.dot(rot,self.data())
-        self.x, self.y = xy[0],xy[1]
+        xy = np.dot(rot, self.data())
+        self.x, self.y = xy[0], xy[1]
         # reset label
         self.set_labels()
         

splitwavepy/core/trio.py
@@ -145,7 +145,7 @@ class Trio:
 
     def rotz(self,degs):
         """Rotate about z axis."""
-        rads = np.degs2rad(degs)
+        rads = math.radians(degs)
         rotateto( geom.rz( self.cmpvecs,rads))
         
     # Windowing